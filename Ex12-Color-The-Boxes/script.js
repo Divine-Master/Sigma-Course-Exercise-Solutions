72652298bdd067645573eafde03aa52fca6e2caf
DivineMaster <divine-master7@outlook.com>
2023-12-26 23:51:42 +0530

    updated ex 12 solution

--- Ex12-Color-The-Boxes/script.js
+++ Ex12-Color-The-Boxes/script.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll(".box").forEach(e => {
-    let arrColor = ['orange', 'green', 'blue', 'red', 'yellow', 'cyan', 'aqua', 'teal', 'pink', 'purple'];
+let arrColor = ['orange', 'green', 'blue', 'red', 'yellow', 'cyan', 'aqua', 'teal', 'pink', 'purple'];
+
+function generateRandom(a) {
     let randomnum = Math.floor(Math.random() * 10);
-    let newColor = arrColor[randomnum];
-    e.style.backgroundColor = newColor;
-    let randomnum2 = Math.floor(Math.random() * 10);
-    let newColor2 = arrColor[randomnum2];
-    e.style.color = newColor2;
-  });
-});
+    return a[randomnum];
+}
+
+window.onload = (e=>{
+    document.querySelectorAll(".box").forEach(e => {
+        e.style.backgroundColor = generateRandom(arrColor);
+        e.style.color = generateRandom(arrColor);
+    });
+})
