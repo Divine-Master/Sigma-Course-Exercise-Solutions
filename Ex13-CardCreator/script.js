92ddbb315b2483f2e3aba98479c47e7701f9d9d4
Replit user <>
2024-01-28 17:02:35 +0000

    ex13

--- Ex13-CardCreator/script.js
+++ Ex13-CardCreator/script.js
@@ -1,22 +1,19 @@
 function createCard(title, cName, Acviews, monthsOld, duration, thumbnail) {
-    title = prompt('Enter title');
-    cName = prompt('Enter Channel name')
-    Acviews = parseInt(prompt('Enter no. of views'))
-    monthsOld = prompt('Enter Months')
-    duration = prompt('Enter duration')
-    thumbnail = prompt('Enter the thumbnail src')
-    let viewStr
-    if (Acviews < 1000) {
-        viewStr = Acviews;
-    }
-    else if (Acviews > 1000000) {
-        viewStr = Acviews / 1000000 + "M";
-    }
-    else {
-        viewStr = Acviews / 1000 + "K";
-    }
-    let viewApx = Math.floor(viewStr)
-    let html = `<div class="card">
+  title = prompt("Enter title");
+  cName = prompt("Enter Channel name");
+  Acviews = parseInt(prompt("Enter no. of views"));
+  monthsOld = prompt("Enter Months");
+  duration = prompt("Enter duration");
+  thumbnail = prompt("Enter the thumbnail src");
+  let viewStr;
+  if (Acviews < 1000) {
+    viewStr = Math.floor(Acviews);
+  } else if (Acviews > 1000000) {
+    viewStr = Math.floor(Acviews / 1000000) + "M";
+  } else {
+    viewStr = Math.floor(Acviews / 1000) + "K";
+  }
+  let html = `<div class="card">
             <div class="image">
                 <img src="${thumbnail}" alt="" />
                 <div class="capsule">${duration}</div>
@@ -25,10 +22,9 @@ function createCard(title, cName, Acviews, monthsOld, duration, thumbnail) {
                 <h4>${title}</h4>
                 <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
             </div>
-        </div>`
+        </div>`;
 
-    document.querySelector('.container').innerHTML += html
+  document.querySelector(".container").innerHTML += html;
 }
-
 
 // createCard("Introductiv video #2", "CWH", 5600000000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
