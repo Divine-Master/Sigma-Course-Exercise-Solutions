5adb29974b50466568a4042f5f62a7f908f1d88b
Divine-Master <divine-master1@outlook.com>
2024-06-05 01:35:17 +0530

    Ex18-CardCreator-React

--- React/Ex18-CardCreator-React/src/components/Card.jsx
+++ React/Ex18-CardCreator-React/src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({content}) => {
       <div className="card border-4 border-black rounded-xl md:rounded-3xl p-3 md:p-6 flex flex-col justify-center align-middle m-5 gap-1.5 bg-gray-800">
         <h2 className="title text-2xl md:text-3xl font-bold m-auto underline">{content.title}</h2>
         <p className="md:text-2xl font-mono m-auto">{content.body}</p>
-        <div class="other flex justify-between">
+        <div className="other flex justify-between">
           <span>{content.id}</span><span>{content.userId}</span>
         </div>
       </div>
